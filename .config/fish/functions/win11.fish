68751970e96dbde86b8eeabea2e446c4d5fd6fa2
LiFubin <lifubinmx@qq.com>
2025-07-24 21:51:19 +0800

    update

---
+++ .config/fish/functions/win11.fish
@@ -0,0 +1,4 @@
+function win11 --wraps='xfreerdp /v:172.16.20.93 /u:imngod@outlook.com /p:Skgx_5689 /f' --wraps='xfreerdp /v:172.16.20.93 /u:imngod@outlook.com /f' --wraps='xfreerdp /v:172.16.20.93 /u:imngod@outlook.com /f /scale:140' --description 'alias win11 xfreerdp /v:172.16.20.93 /u:imngod@outlook.com /f /scale:140'
+  xfreerdp /v:172.16.20.93 /u:imngod@outlook.com /f /scale:140 $argv
+        
+end
